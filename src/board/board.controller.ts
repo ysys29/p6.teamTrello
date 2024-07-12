2952ab63f71b05b950d1ebe388d483a8fece70e8
yooseungyeop <yooseungyub@naver.com>
2024-07-12 17:47:20 +0900

    feat : make GetBoardSangse

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -2,12 +2,13 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dtos/create-board.dto';
 import { UpdateBoardDto } from './dtos/update-board.dto';
+import { Board } from './entities/board.entity';
 
 @Controller('boards')
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
 
-  @Post()
+  @Post() // 요청 본문에 포함된 데이터를 사용하여 새보드를 생성, 이를 데이터베이스에 저장합니다.
   async create(@Body() createBoardDto: CreateBoardDto) {
     return this.boardService.create(createBoardDto);
   }
@@ -17,9 +18,9 @@ export class BoardController {
     return this.boardService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.boardService.findOne(+id);
+  @Get(':id') //URL의 매개변수로 전달된 ID를 사용하여 보드 객체를 조회
+  async findOne(@Param('id') id: number): Promise<Board> {
+    return this.boardService.findOne(id);
   }
 
   @Patch(':id')
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateBoardDto } from './dtos/create-board.dto';
 import { UpdateBoardDto } from './dtos/update-board.dto';
 import { Repository } from 'typeorm';
@@ -14,22 +14,34 @@ export class BoardService {
 
   // 보드 생성
   async create(createBoardDto: CreateBoardDto): Promise<Board> {
+    // DTO를 사용하여 새 보드 객체 생성
     const board = this.boardRepository.create(createBoardDto);
+    // 생성된 보드 객체를 데이터베이스에 저장
     return this.boardRepository.save(board);
   }
-
+  //보드목록검색 추가구현 사항임 일단은
   findAll() {
     return `This action returns all board`;
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} board`;
+  // 보드 상세 조회
+  async findOne(id: number): Promise<Board> {
+    // 주어진 ID로 보드 객체를 조회
+    const board = await this.boardRepository.findOne({
+      where: { id },
+      relations: ['user', 'boardMembers', 'lists'],
+      //보드 멤버들이랑 리스트들도 받아옴
+    });
+    // 보드가 존재하지 않으면 에러메세지 뱉어냄 id없으면
+    if (!board) {
+      throw new NotFoundException(`존재하지 않는 보드입니다.`);
+    }
+    return board;
   }
-
+  //보드 수정
   update(id: number, updateBoardDto: UpdateBoardDto) {
     return `This action updates a #${id} board`;
   }
-
+  //보드 삭제
   remove(id: number) {
     return `This action removes a #${id} board`;
   }
