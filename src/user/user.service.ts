067255044a7204aaece6718092afc9d1550ac358
이강산 <gangsan1414@gmail.com>
2024-07-12 16:16:25 +0900

    feat/인증인가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,21 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  findAll() {
-    return `This action returns all user`;
-  }
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOneById(userId: number) {
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+      relations: ['boardMembers'],
+    });
+
+    if (!user || user.deletedAt !== null) {
+      throw new NotFoundException('사용자를 찾을 수 없습니다.');
+    }
+
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
+    // 사용자
+
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  // 회원 탈퇴하기
+  async softDelete(id: number) {
+    const user = await this.findOneById(id);
+
+    await this.userRepository.softDelete({ id });
   }
 }
