7b41427ed46842bb53233c7d03a8bc2fb2135fed
yooseungyeop <yooseungyub@naver.com>
2024-07-13 18:37:51 +0900

    feat : board삭제, token인증 추가

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,35 +1,51 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dtos/create-board.dto';
 import { UpdateBoardDto } from './dtos/update-board.dto';
 import { Board } from './entities/board.entity';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
+@ApiTags('보드')
 @Controller('boards')
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
 
-  @Post() // 요청 본문에 포함된 데이터를 사용하여 새보드를 생성, 이를 데이터베이스에 저장합니다.
-  async create(@Body() createBoardDto: CreateBoardDto) {
-    return this.boardService.create(createBoardDto);
+  @ApiBearerAuth() // 애가 토큰인증해주는애
+  @UseGuards(AuthGuard('jwt')) // 모든 엔드포인트에 대해 JWT 인증 적용
+  @Post() // 요청 본문에 포함된 데이터를 사용하여 새 보드를 생성, 이를 데이터베이스에 저장합니다.
+  async create(@Body() createBoardDto: CreateBoardDto, @Request() req) {
+    const userId = req.user.id; // JWT 토큰에서 사용자 ID 추출
+    return this.boardService.create(createBoardDto, userId);
   }
-
+  // 보드에 멤버가 추가되면 추후에 작성 예정
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   findAll() {
     return this.boardService.findAll();
   }
 
-  @Get(':id') //URL주소에 전달된 ID를 사용하여 해당 보드를 조회
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @Get(':id') // URL주소에 전달된 ID를 사용하여 해당 보드를 조회
   async findOne(@Param('id') id: number): Promise<Board> {
     return this.boardService.findOne(id);
   }
 
-  @Patch(':id') //URL주소에 전달된 ID를 사용하여 해당 보드를 수정
-  update(@Param('id') id: string, @Body() updateBoardDto: UpdateBoardDto) {
-    return this.boardService.update(Number(id), updateBoardDto);
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @Patch(':id') // URL주소에 전달된 ID를 사용하여 해당 보드를 수정
+  async update(@Param('id') id: string, @Body() updateBoardDto: UpdateBoardDto, @Request() req) {
+    const userId = req.user.id; // JWT 토큰에서 사용자 ID 추출
+    return this.boardService.update(Number(id), updateBoardDto, userId);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.boardService.remove(+id);
+  async remove(@Param('id') id: string, @Request() req) {
+    const userId = req.user.id; // JWT 토큰에서 사용자 ID 추출
+    return this.boardService.remove(Number(id), userId);
   }
 }
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -13,32 +13,37 @@ export class BoardService {
   ) {}
 
   // 보드 생성
-  async create(createBoardDto: CreateBoardDto): Promise<Board> {
+  async create(createBoardDto: CreateBoardDto, userId: number): Promise<Board> {
     // DTO를 사용하여 새 보드 객체 생성
-    const board = this.boardRepository.create(createBoardDto);
+    const board = this.boardRepository.create({ ...createBoardDto, ownerId: userId });
     // 생성된 보드 객체를 데이터베이스에 저장
     return this.boardRepository.save(board);
   }
-  //보드목록검색 추가구현 사항임 일단은
+
+  // 보드 목록 검색 추가 구현 사항, 일단은
   findAll() {
     return `This action returns all board`;
   }
+
   // 보드 상세 조회
   async findOne(id: number): Promise<Board> {
     // 주어진 ID로 보드 객체를 조회
     const board = await this.boardRepository.findOne({
       where: { id },
       relations: ['user', 'boardMembers', 'lists'],
-      //보드 멤버들이랑 리스트들도 받아옴
+      // 보드 멤버들과 리스트들도 받아옴
     });
-    // 보드가 존재하지 않으면 에러메세지 뱉어냄 id없으면
+
+    // 보드가 존재하지 않으면 에러 메시지 출력
     if (!board) {
       throw new NotFoundException(`존재하지 않는 보드입니다.`);
     }
+
     return board;
   }
-  //보드 수정
-  async update(id: number, updateBoardDto: UpdateBoardDto): Promise<Board> {
+
+  // 보드 수정
+  async update(id: number, updateBoardDto: UpdateBoardDto, userId: number): Promise<Board> {
     // 주어진 ID로 보드 객체를 조회
     const board = await this.boardRepository.findOne({ where: { id } });
 
@@ -47,10 +52,10 @@ export class BoardService {
       throw new NotFoundException(`존재하지 않는 보드입니다.`);
     }
 
-    // // 보드 생성자가 아니면 에러
-    // if (board.ownerId !== userId) {
-    //   throw new UnauthorizedException('보드수정은 생성자만 가능합니다.');
-    // }
+    // 보드의 소유자가 아닌 경우 예외 발생
+    if (board.ownerId !== userId) {
+      throw new UnauthorizedException('보드를 수정할 권한이 없습니다.');
+    }
 
     // 보드 객체 업데이트
     Object.assign(board, updateBoardDto);
@@ -58,8 +63,20 @@ export class BoardService {
     // 업데이트된 보드 객체를 데이터베이스에 저장
     return this.boardRepository.save(board);
   }
-  //보드 삭제
-  remove(id: number) {
-    return `This action removes a #${id} board`;
+
+  // 보드 삭제
+  async remove(id: number, userId: number): Promise<void> {
+    const board = await this.boardRepository.findOne({ where: { id } });
+
+    if (!board) {
+      throw new NotFoundException(`존재하지 않는 보드입니다.`);
+    }
+
+    // 보드의 소유자가 아닌 경우 예외 발생
+    if (board.ownerId !== userId) {
+      throw new UnauthorizedException('보드를 삭제할 권한이 없습니다.');
+    }
+
+    await this.boardRepository.remove(board);
   }
 }
