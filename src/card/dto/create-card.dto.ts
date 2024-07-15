137e6e2b6cdc75c1ebd8349593da8234a78ad0ed
SW-64 <ettjddns6@gmail.com>
2024-07-15 20:32:29 +0900

    Fix / createCarddto파일  IsNotEmpty 추가

--- src/card/dto/create-card.dto.ts
+++ src/card/dto/create-card.dto.ts
@@ -1,15 +1,19 @@
-import { IsString, IsHexColor, IsNumber } from 'class-validator';
+import { IsString, IsHexColor, IsNumber, IsNotEmpty } from 'class-validator';
 
 export class CreateCardDto {
+  @IsNotEmpty({ message: `colistIdlor 입력해주세요` })
   @IsNumber()
   listId: number;
 
+  @IsNotEmpty({ message: `title 입력해주세요` })
   @IsString()
   title: string;
 
+  @IsNotEmpty({ message: `content 입력해주세요` })
   @IsString()
   content: string;
 
+  @IsNotEmpty({ message: `color 입력해주세요` })
   @IsHexColor({ message: '올바른 색상 코드를 입력해 주세오' })
   color: string;
 }
