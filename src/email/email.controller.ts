05b365412192b614745c4bae71eb309af7627aba
이강산 <gangsan1414@gmail.com>
2024-07-15 13:50:22 +0900

    bugfix:2번씩 실행되는 오류 수정

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { ApiTags } from '@nestjs/swagger';
 import { IsValidEmailDto } from './dtos/is-valid-email.dto';
+import { Cron } from '@nestjs/schedule';
 
 @ApiTags('이메일')
 @Controller('emails')
@@ -22,5 +23,11 @@ export class EmailController {
       message: `${isValidEmailDto.email}에 인증 토큰을 보냈습니다.`,
       data: data,
     };
+  }
+
+  // 1분마다 실행. 정확히는 0초일때마다 실행
+  @Cron('0 * * * * *')
+  async handleCron() {
+    await this.emailService.deleteOldEmails();
   }
 }
