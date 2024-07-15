093da457ef1084d05243e6181db827622e9a41bb
lys <projectys29@gmail.com>
2024-07-15 12:35:25 +0900

    chore: swagger 예시 추가

--- src/invitation/dtos/invitation-id.dto.ts
+++ src/invitation/dtos/invitation-id.dto.ts
@@ -2,6 +2,10 @@ import { Type } from 'class-transformer';
 import { IsNumber } from 'class-validator';
 
 export class InvitationIdDto {
+  /**
+   * 초대 id
+   * @example 1
+   */
   @IsNumber()
   @Type(() => Number)
   invitationId: number;
--- src/list/dtos/list-id.dto.ts
+++ src/list/dtos/list-id.dto.ts
@@ -2,6 +2,10 @@ import { Type } from 'class-transformer';
 import { IsNumber } from 'class-validator';
 
 export class ListIdDto {
+  /**
+   * 리스트 id
+   * @example 1
+   */
   @IsNumber()
   @Type(() => Number)
   listId: number;
