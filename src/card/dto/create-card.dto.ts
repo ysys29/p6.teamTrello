9b45c2fbad7a4d10dacb2fdd4476d29231efca6e
SW-64 <ettjddns6@gmail.com>
2024-07-16 20:08:12 +0900

    update : color 기본값 설정

--- src/card/dto/create-card.dto.ts
+++ src/card/dto/create-card.dto.ts
@@ -1,18 +1,30 @@
 import { IsString, IsHexColor, IsNumber, IsNotEmpty } from 'class-validator';
 
 export class CreateCardDto {
+  /**
+   * @example 1
+   */
   @IsNotEmpty({ message: `리스트 아이디를 입력해주세요` })
   @IsNumber()
   listId: number;
 
+  /**
+   * @example 튜터님께 여쭤보기
+   */
   @IsNotEmpty({ message: `제목을 입력해주세요` })
   @IsString()
   title: string;
 
+  /**
+   * @example 뭐라고 여쭤보지
+   */
   @IsNotEmpty({ message: `내용을 입력해주세요` })
   @IsString()
   content: string;
 
+  /**
+   * @example #ffffff
+   */
   @IsNotEmpty({ message: `색상 코드를 입력해주세요` })
   @IsHexColor({ message: '올바른 색상 코드를 입력해 주세요' })
   color: string;
--- src/card/dto/update-card.dto.ts
+++ src/card/dto/update-card.dto.ts
@@ -1,18 +1,30 @@
 import { IsOptional, IsString, IsHexColor, IsNumber, IsDate } from 'class-validator';
 import { Type } from 'class-transformer';
 export class UpdateCardDto {
+  /**
+   * @example 1
+   */
   @IsOptional()
   @IsNumber()
   listId?: number;
 
+  /**
+   * @example 튜터님께 여쭤보기
+   */
   @IsOptional({ message: '타이틀을 입력해 주세요.' })
   @IsString()
   title?: string;
 
+  /**
+   * @example 뭐라고 여쭤보지
+   */
   @IsOptional()
   @IsString()
   content?: string;
 
+  /**
+   * @example #ffffff
+   */
   @IsOptional()
   @IsHexColor({ message: '올바른 색상 코드를 입력해 주세오' })
   color?: string;
